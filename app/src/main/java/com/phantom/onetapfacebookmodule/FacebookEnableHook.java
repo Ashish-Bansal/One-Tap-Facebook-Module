3eeb4a1376edec06510fe81e986e2218956cc790
Ashish Bansal <bansal.ashish096@gmail.com>
2016-09-09 02:00:32 +0530

    Removed Facebook Media Hooking
    This project has been merged into One Tap Video Download. Just for the
    sake of the consistency as well as to continue the legacy, this *empty*
    module will still be kept and users will have to install it to enable
    facebook video downloading from the main project(One Tap Video Download).

---
+++ app/src/main/java/com/phantom/onetapfacebookmodule/FacebookEnableHook.java
@@ -0,0 +1,9 @@
+package com.phantom.onetapfacebookmodule;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class FacebookEnableHook implements IXposedHookLoadPackage {
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    }
+}
